8111f4b7e9db63c8362b0372ddb43756512256bd
Sahib.Aujla <sahibpreet568@gmail.com>
2024-12-23 14:42:39 -0500

    added chat.tsx

--- frontend/src/components/Content.tsx
+++ frontend/src/components/Content.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import Chat from "./Chat";
 
 const Content = () => {
   return (
     <div className="w-full text-white flex flex-row">
       <div className="w-1/6 border-r-2 h-full border-gray-400">Sidebar</div>
-      <div className="w-5/6">Chat shit</div>
+      <div className="w-5/6">
+        <Chat />
+      </div>
     </div>
   );
 };
