86768e59437659ec24bf02e75813c2fe35c0b141
Sahib.Aujla <sahibpreet568@gmail.com>
2024-12-24 07:50:42 -0500

    added messages

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+
+interface Message {
+  isUser: boolean;
+  message: string;
+  time: number;
+}
 const Chat = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [userMessage, setUserMessage] = useState("");
+  const handleClick = () => {
+    if (!userMessage) return;
+    const m: Message = {
+      isUser: true,
+      message: userMessage,
+      time: Date.now(),
+    };
+    setMessages((prev) => [...prev, m]);
+  };
   return (
     <div className="w-full h-[80vh] ">
-      <div className="w-full h-full flex justify-center items-center">
-        <div>Messagess</div>
+      <div className="w-full h-full flex flex-col-reverse px-20 py-5">
+        {messages.map((message) => (
+          <div
+            className={`w-full flex ${
+              message.isUser ? "justify-end" : "justify-start"
+            }`}
+          >
+            {message.message}
+          </div>
+        ))}
       </div>
       <div className="absolute bottom-8 w-3/4 h-11 flex justify-center items-center">
         <div className="  w-1/2 h-10 flex z-10">
@@ -10,8 +36,13 @@ const Chat = () => {
             className="border-none bg-slate-500 w-3/4 h-full text-white focus:outline-0  z-10 p-4 rounded-md"
             placeholder="Likh fer ....."
             type="text"
+            value={userMessage}
+            onChange={(e) => setUserMessage(e.target.value)}
           />
-          <button className="ml-2 p-2 h-full w-35 rounded-xl bg-slate-500 shadow hover:bg-slate-400  font-semibold transition-all ease-in-out  z-10">
+          <button
+            onClick={handleClick}
+            className="ml-2 p-2 h-full w-35 rounded-xl bg-slate-500 shadow hover:bg-slate-400  font-semibold transition-all ease-in-out  z-10"
+          >
             Bhejo
           </button>
         </div>
